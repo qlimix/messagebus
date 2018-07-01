34b32dcbfec5123777b447ef727db1249932db5b
frank <frank@qlimix.net>
2018-07-01 13:40:23 +0200

    forgot to change the default in the implementation

--- src/Locator/InMemoryHandlerRegistry.php
+++ src/Locator/InMemoryHandlerRegistry.php
@@ -13,7 +13,7 @@ final class InMemoryHandlerRegistry implements HandlerRegistryInterface
     /**
      * @inheritDoc
      */
-    public function register(string $handler, string $messageName, string $method = 'handle'): void
+    public function register(string $handler, string $messageName, string $method = 'execute'): void
     {
         $this->handlers[$messageName] = new Handler($handler, $method);
     }
