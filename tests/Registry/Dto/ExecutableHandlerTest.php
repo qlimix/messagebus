410bf2d9808872a23988b4cb8a9715bcc11618cd
frank <frank@qlimix.net>
2019-06-22 22:21:10 +0200

    extra tests for dto

---
+++ tests/Registry/Dto/ExecutableHandlerTest.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace Qlimix\Tests\MessageBus\Registry\Dto;
+
+use PHPUnit\Framework\TestCase;
+use Qlimix\MessageBus\Locator\Dto\ExecutableHandler;
+use stdClass;
+
+final class ExecutableHandlerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldDto(): void
+    {
+        $object = new stdClass();
+        $method = 'foo';
+
+        $executableHandler = new ExecutableHandler($object, $method);
+
+        $this->assertSame($object, $executableHandler->getHandler());
+        $this->assertSame($method, $executableHandler->getMethod());
+    }
+}
